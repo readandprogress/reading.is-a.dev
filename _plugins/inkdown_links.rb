5333e7324885d614d0627898fea798f6f9688e9c
Reading <readandprogress@duck.com>
2025-05-28 22:13:50 +0000

    Added a script to convert Inkdown links.

---
+++ _plugins/inkdown_links.rb
@@ -0,0 +1,22 @@
+require 'jekyll'
+
+module Jekyll
+  class InkdownLinks < Generator
+    safe true
+
+    def generate(site)
+      site.pages.each do |page|
+        page.content = process_links(page.content)
+      end
+    end
+
+    def process_links(content)
+      content.gsub!(/$$([^$$]+)\]$Media%2F([^)]*)\.md$/) do |match|
+        title = $1
+        path = $2.gsub('%20', ' ').gsub('%2F', '/')
+        "<a href='/_tabs/Media/#{path}/'>#{title}</a>"
+      end
+      content
+    end
+  end
+end
