21d520eb0cec241d09cd0b7985f0a05ebb5d87f4
Reading <readandprogress@duck.com>
2025-09-18 23:07:05 +0000

    More testing

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758236043',resources: [
+    cacheName: 'chirpy-1758236432',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
