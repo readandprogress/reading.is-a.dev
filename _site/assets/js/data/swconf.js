0d8d9b4285ef050b7c24146002422bb11913b194
Reading <readandprogress@duck.com>
2025-09-18 22:55:40 +0000

    Finished reimplementing some features, now transferring images.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758234362',resources: [
+    cacheName: 'chirpy-1758236043',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
